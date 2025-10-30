9c5b7fab1a5414f0484c032f392f9b5251b10edb
Bruno Henrique <155691067+brunohenriquedeveloper@users.noreply.github.com>
2025-10-30 19:46:10 -0300

    Feat: Starting creating the first req and res for my backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,2 +1,26 @@
 import express from 'express'
 import cors from 'cors'
+
+async function main () {
+    const hostname = 'localhost'
+    const port = 3000
+
+    const app = express()
+
+    app.use(express.json()) // arruma a resposta do servidor
+    app.use(cors())
+
+    app.get('/', (req, res) => {
+        res.send({
+            success: true,
+            statusCode: 200,
+            body: 'Welcome to the Gastronomy App'
+        })
+    })
+
+    app.listen(port, () => {
+        console.log(`Server running on: http://${hostname}:${port}`)
+    })
+}
+
+main()
