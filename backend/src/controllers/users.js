fa367358f6e746c438148f5a7d0caf45b92b0ab9
Bruno Henrique <155691067+brunohenriquedeveloper@users.noreply.github.com>
2025-11-05 11:46:20 -0300

    Feat: implementing the Controller using the Response pattern

---
+++ backend/src/controllers/users.js
@@ -0,0 +1,18 @@
+import UsersDataAccess from "../dataAccess/users.js"
+import { ok, serverError } from '../helpers/httpResponse.js'
+
+export default class UsersControllers{
+    constructor() {
+        this.dataAccess = new UsersDataAccess()
+    }
+
+    async getUsers(){
+        try{
+            const users = this.dataAccess.getUsers()
+
+            return ok(users)
+        } catch(error){
+            return serverError(error)
+        }
+    }
+}
