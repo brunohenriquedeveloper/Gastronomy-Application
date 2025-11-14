6ef518d3c494b13fe017b929a17c97661ab09dc7
Bruno Henrique <155691067+brunohenriquedeveloper@users.noreply.github.com>
2025-11-14 00:20:28 -0300

    Feat: Creating the NavBar with No Responsivity (FOR NOW)

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -1,10 +1,18 @@
 import styles from './navbar.module.css'
+import { LuShoppingCart, LuUser } from "react-icons/lu";
 
 export default function Navbar(){
     return(
         <nav className={styles.navbarContainer}>
             <div className={styles.navbarItems}>
+                <img src="/logo-Places.png" alt=""  className={styles.logo}/>
+            </div>
 
+            <div className={styles.navbarLinksContainer}>
+            <a href="" className={styles.navbarLink}>Home</a>
+            <a href="" className={styles.navbarLink}>Places</a>
+            <LuShoppingCart className={styles.navbarLink}/>
+            <LuUser className={styles.navbarLink}/>
             </div>
         </nav>
     )
