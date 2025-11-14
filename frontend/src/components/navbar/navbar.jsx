1cecf43c332077ff0868c2ccdcd274b2282aa052
Bruno Henrique <155691067+brunohenriquedeveloper@users.noreply.github.com>
2025-11-14 00:31:44 -0300

    Feat: Creating a responsible navbar

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -1,12 +1,12 @@
 import styles from './navbar.module.css'
-import { LuShoppingCart, LuUser } from "react-icons/lu";
+import { LuMenu, LuShoppingCart, LuUser } from "react-icons/lu";
 
 export default function Navbar(){
     return(
         <nav className={styles.navbarContainer}>
             <div className={styles.navbarItems}>
                 <img src="/logo-Places.png" alt=""  className={styles.logo}/>
-            </div>
+            
 
             <div className={styles.navbarLinksContainer}>
             <a href="" className={styles.navbarLink}>Home</a>
@@ -14,6 +14,16 @@ export default function Navbar(){
             <LuShoppingCart className={styles.navbarLink}/>
             <LuUser className={styles.navbarLink}/>
             </div>
+        </div>
+
+        <div className={styles.mobileNavBarItems}>
+        <img src="/logo-Places.png" alt=""  className={styles.logo}/>
+        <div className={styles.mobileNavBarButtons}>
+            <LuShoppingCart className={styles.navbarLink}/>
+            <LuMenu className={styles.navbarLink}/>
+        </div>
+        </div>
+            
         </nav>
     )
 }
