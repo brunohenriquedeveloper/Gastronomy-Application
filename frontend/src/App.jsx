72ba3a72c6bfb59f9cf97fbe30d1a3f1fefd70bc
Bruno Henrique <155691067+brunohenriquedeveloper@users.noreply.github.com>
2025-11-13 23:56:36 -0300

    Feat: Creating the navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
-
+import Navbar from "./components/navbar/navbar"
 
 
 
 export default function App() {
   return (
     <>
-      <h1 style={{background: 'gray', maxWidth: 'fit-content'}}>My Places</h1>
-      <p>Testando um body</p>
+    <Navbar />
     </>
   )
 }
---
+++ frontend/src/components/navbar/navbar.jsx
@@ -0,0 +1,11 @@
+import styles from './navbar.modules.css'
+
+export default function Navbar(){
+    return(
+        <nav className={styles.navbarContainer}>
+            <div className={styles.navbarItems}>
+
+            </div>
+        </nav>
+    )
+}
