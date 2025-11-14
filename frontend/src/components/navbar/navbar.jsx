3204a69d00f1e5fc43f8d93c11f2241940f3d1df
Bruno Henrique <155691067+brunohenriquedeveloper@users.noreply.github.com>
2025-11-14 00:53:16 -0300

    Style: Adjusting Style and Implementing a responsible navbar button

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -1,7 +1,16 @@
 import styles from './navbar.module.css'
 import { LuMenu, LuShoppingCart, LuUser } from "react-icons/lu";
+import {Drawer} from '@mui/material'
+import { useState } from 'react';
 
 export default function Navbar(){
+    const [openMenu, setOpenMenu] = useState(false)
+
+    const handleOpenMenu = () => {
+        setOpenMenu(!openMenu)
+    }
+
+
     return(
         <nav className={styles.navbarContainer}>
             <div className={styles.navbarItems}>
@@ -20,9 +29,20 @@ export default function Navbar(){
         <img src="/logo-Places.png" alt=""  className={styles.logo}/>
         <div className={styles.mobileNavBarButtons}>
             <LuShoppingCart className={styles.navbarLink}/>
-            <LuMenu className={styles.navbarLink}/>
+            <LuMenu className={styles.navbarLink} onClick={handleOpenMenu}/>
         </div>
         </div>
+        <Drawer
+        anchor='right'
+        open={openMenu}
+        onClose={handleOpenMenu}
+        >
+        <div className={styles.drawer}>
+            <a href="" className={styles.navbarLink}>Home</a>
+            <a href="" className={styles.navbarLink}>Places</a>
+            <a href="" className={styles.navbarLink}>Profile</a>
+        </div>
+        </Drawer>
             
         </nav>
     )
