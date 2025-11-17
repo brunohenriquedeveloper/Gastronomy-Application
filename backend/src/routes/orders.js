9955bd0c1c14f873af1c426581c1a919037c72c6
Bruno Henrique <155691067+brunohenriquedeveloper@users.noreply.github.com>
2025-11-17 12:09:12 -0300

    Chore: Renaming the GET order by id router

--- backend/src/routes/orders.js
+++ backend/src/routes/orders.js
@@ -13,7 +13,7 @@ ordersRouter.get('/', async(req, res) => {
     res.status(200).send({success, statusCode, body})
 })
 
-ordersRouter.get('/:id', async(req, res) => {
+ordersRouter.get('/userorders/:id', async(req, res) => {
     const { success, statusCode, body } = await ordersControllers.getOrdersByUserId(req.params.id)
 
     res.status(200).send({success, statusCode, body})
