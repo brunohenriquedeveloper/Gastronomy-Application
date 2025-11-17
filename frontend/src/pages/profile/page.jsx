b327da88ef2e97b198dd2876f8b99ff8150effbe
Bruno Henrique <155691067+brunohenriquedeveloper@users.noreply.github.com>
2025-11-17 12:54:26 -0300

    Style: Stylizing the initial profile page

--- frontend/src/pages/profile/page.jsx
+++ frontend/src/pages/profile/page.jsx
@@ -1,30 +1,72 @@
 import { useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 import authServices from "../../services/auth"
+import orderServices from "../../services/order"
+import styles from "./page.module.css"
 
 export default function Profile(){
 
     const {logout } = authServices()
+    const { getUsersOrders, orderLoading, refetchOrders, ordersList} = orderServices()
     const navigate = useNavigate()
     const authData = JSON.parse(localStorage.getItem('auth'))
 
     useEffect(() => {
         if(!authData){
             navigate('/auth')
+        } else if( refetchOrders){
+            getUsersOrders(authData?.user?._id)
         }
-    }, [authData, navigate])
+
+
+    }, [authData, navigate, refetchOrders])
+
+    if(orderLoading){
+        return (<h1>Loading...</h1>)
+    }
 
     const handleLogout =  () =>{
         logout()
         return navigate('/')
     }
 
+    console.log(ordersList)
+
     return (
-        <>
-        <h1>{authData?.user?.fullname}</h1>
-        <h3>{authData?.user?.email}</h3>
+        < div className={styles.pageContainer}>
+
+            <div>
+            <h1>{authData?.user?.fullname}</h1>
+            <h3>{authData?.user?.email}</h3>
+            </div>
+        
         <button onClick={handleLogout}>Logout</button>
-        </>
+
+
+        {ordersList.length > 0 ? 
+
+        <div className={styles.ordersContainer}>
+            {ordersList.map((order) => (
+                <div key={order._id} className={styles.orderContainer}>
+                    <p>{order.pickupStatus}</p>
+                    <h3>{order.pickupTime}</h3>
+                    {order.orderItems.map((item) => (
+                        <div key={item._id}>
+                            <h4>{item.itemDetails[0].name}</h4>
+                            <p>Quantity: {item.quantity}</p>
+                        </div>
+                    ))}
+                </div>
+            ))}
+        </div>
+        
+        :
+        <div className={styles.orderContainer}>
+            You do not have orders yet
+        </div>
+
+        }
+        </div>
         
     )
 }
