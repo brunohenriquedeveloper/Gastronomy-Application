e6b026be15c78e157f4bd62822b47af8a1a740fd
Bruno Henrique <155691067+brunohenriquedeveloper@users.noreply.github.com>
2025-11-30 16:00:16 -0300

    Feat: Implementing a new category

--- frontend/src/pages/home/page.jsx
+++ frontend/src/pages/home/page.jsx
@@ -12,40 +12,59 @@ export default function Home(){
             </section>
 
     <section className={styles.placesSection}>
-    <div>
+    <div className={styles.placesSectionDiv}>
         <img src="/imgs/homepage/paris.avif" alt="Europe" className={styles.image} />
-        <FaMapMarkerAlt />
-        <h4>Europe</h4>
+        <div className={styles.titleWithIcon}>
+            <FaMapMarkerAlt className={styles.icon} />
+            <h4>Europe</h4>
+        </div>
         <p>Explore historic cities, diverse cultures, and stunning landscapes.</p>
     </div>
 
-    <div>
+    <div className={styles.placesSectionDiv}>
         <img src="/imgs/homepage/america.jpg" alt="Americas" className={styles.image} />
-        <FaMapMarkerAlt />
-        <h4>Americas</h4>
-        <p>From Patagonia to Alaska — nature, culture, and unforgettable adventures.</p>
+        <div className={styles.titleWithIcon}>
+            <FaMapMarkerAlt className={styles.icon} />
+            <h4>Americas</h4>
+        </div>
+        <p>From Patagonia to Alaska. Nature, culture, and unforgettable adventures.</p>
     </div>
 
-    <div>
+    <div className={styles.placesSectionDiv}>
         <img src="/imgs/homepage/japan.jpg" alt="Asia" className={styles.image} />
-        <FaMapMarkerAlt />
-        <h4>Asia</h4>
+        <div className={styles.titleWithIcon}>
+            <FaMapMarkerAlt className={styles.icon} />
+            <h4>Asia</h4>
+        </div>
         <p>Ancient traditions meet futuristic innovation across vibrant landscapes.</p>
     </div>
 
-    <div>
+    <div className={styles.placesSectionDiv}>
         <img src="/imgs/homepage/africa.jpg" alt="Africa" className={styles.image} />
-        <FaMapMarkerAlt />
-        <h4>Africa</h4>
+        <div className={styles.titleWithIcon}>
+            <FaMapMarkerAlt className={styles.icon} />
+            <h4>Africa</h4>
+        </div>
         <p>Safaris, deserts, forests, and extraordinary cultural richness.</p>
     </div>
 
-    <div>
+    <div className={styles.placesSectionDiv}>
         <img src="/imgs/homepage/oceania.jpg" alt="Oceania" className={styles.image} />
-        <FaMapMarkerAlt />
-        <h4>Oceania</h4>
+        <div className={styles.titleWithIcon}>
+            <FaMapMarkerAlt className={styles.icon} />
+            <h4>Oceania</h4>
+        </div>
         <p>Paradise beaches, exotic nature, and unique experiences await.</p>
     </div>
+
+ <div className={styles.placesSectionDiv}>
+    <img src="/imgs/homepage/discover.jpg" alt="Discover" className={styles.image} />
+    <div className={styles.titleWithIcon}>
+        <FaMapMarkerAlt className={styles.icon} />
+        <h4>Discover New Places</h4>
+    </div>
+    <p>Find more destinations around the world.</p>
+</div>
 </section>
 
     <section className={styles.contactSection}>
