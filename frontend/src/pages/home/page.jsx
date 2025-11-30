e6c78037a48398033446ebae23c725ba236a6c4a
Bruno Henrique <155691067+brunohenriquedeveloper@users.noreply.github.com>
2025-11-30 15:30:25 -0300

    Feat: Implementing the home content section

--- frontend/src/pages/home/page.jsx
+++ frontend/src/pages/home/page.jsx
@@ -11,31 +11,44 @@ export default function Home(){
                 </p>
             </section>
 
-            <section className={styles.placesSection}>
-                <div>
-                    <i></i>
-                    <h4></h4>
-                    <p></p>
-                </div>
-            </section>
+    <section className={styles.placesSection}>
+    <div>
+        <img src="/imgs/homepage/paris.avif" alt="Europe" className={styles.image} />
+        <FaMapMarkerAlt />
+        <h4>Europe</h4>
+        <p>Explore historic cities, diverse cultures, and stunning landscapes.</p>
+    </div>
 
-             <section className={styles.placesSection}>
-                <div>
-                    <i></i>
-                    <h4></h4>
-                    <p></p>
-                </div>
-            </section>
+    <div>
+        <img src="/imgs/homepage/america.jpg" alt="Americas" className={styles.image} />
+        <FaMapMarkerAlt />
+        <h4>Americas</h4>
+        <p>From Patagonia to Alaska — nature, culture, and unforgettable adventures.</p>
+    </div>
 
-            <section className={styles.placesSection}>
-                <div>
-                    <i></i>
-                    <h4></h4>
-                    <p></p>
-                </div>
-            </section>
+    <div>
+        <img src="/imgs/homepage/japan.jpg" alt="Asia" className={styles.image} />
+        <FaMapMarkerAlt />
+        <h4>Asia</h4>
+        <p>Ancient traditions meet futuristic innovation across vibrant landscapes.</p>
+    </div>
 
-            <section className={styles.contactSection}>
+    <div>
+        <img src="/imgs/homepage/africa.jpg" alt="Africa" className={styles.image} />
+        <FaMapMarkerAlt />
+        <h4>Africa</h4>
+        <p>Safaris, deserts, forests, and extraordinary cultural richness.</p>
+    </div>
+
+    <div>
+        <img src="/imgs/homepage/oceania.jpg" alt="Oceania" className={styles.image} />
+        <FaMapMarkerAlt />
+        <h4>Oceania</h4>
+        <p>Paradise beaches, exotic nature, and unique experiences await.</p>
+    </div>
+</section>
+
+    <section className={styles.contactSection}>
                 <h1>Stay Updated!</h1>
                 <p>Subscribe to our newsletter to receive the latest updates, also follow us on social media for travel tips directly in your inbox.</p>
                 <div className={styles.socialButtonsContainer}>
